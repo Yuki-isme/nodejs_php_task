4adbe7dab1034e9841ad477f7da26c1f49d149f1
Yuki <phanhoang.isme@gmail.com>
2024-06-20 11:59:42 +0700

    ZingMp3 API

--- src/controllers/ZingMp3Controller.js
+++ src/controllers/ZingMp3Controller.js
@@ -23,7 +23,8 @@ class ZingMp3Controller extends Controller {
             let indexKey = `${item.sectionType}`;
             let content = ``;
             let sectionTypePath = path.join(__dirname, `../views/ZingMp3/sectionType/${item.sectionType}/${item.sectionType}.ejs`);
-            let itemPath = path.join(__dirname, `../views/ZingMp3/sectionType/${item.sectionType}/item.ejs`);
+            let itemsPath = path.join(__dirname, `../views/ZingMp3/sectionType/${item.sectionType}/items.ejs`);
+            let sectionTypeData = {};
             if (typeof item.items !== "undefined") {
                 for (const [key, value] of Object.entries(item.items)) {
                     let data = {}
@@ -34,46 +35,63 @@ class ZingMp3Controller extends Controller {
                         case 'recentPlaylist':
                             break;
                         case 'new-release':
+                            let newReleaseContent = ``;
+                            let itemPath = path.join(__dirname, `../views/ZingMp3/sectionType/${item.sectionType}/item.ejs`);
+                            for (const [newReleaseKey, newReleaseValue] of Object.entries(value)) {
+                                //item.ejs
+                                let newReleaseData = { src: newReleaseValue.thumbnail, href: newReleaseValue.link };
+                                newReleaseContent += await super._renderEjsFile(itemPath, newReleaseData);
+                            }
+                            data = {...data, content: newReleaseContent, key: key};
                             break;
-                        case 'bewReleaseChart':
+                        case 'newReleaseChart':
                             break;
                         case 'RTChart':
                             break;
                         case 'weekChart':
                             break;
                         case 'adBanner':
+                            data = {...data, src: value.thumbnail, href: value.link, title: value.title};
                             break;
                         case 'livestream':
+                            data = {...data, src: value.thumbnail, href: value.link, title: value.title};
                             break;
                         case 'playlist':
-                            data = {...data, title: value.title};
-                            indexKey = `${item.sectionType}_${item.sectionId}`;
                             switch (item.sectionId) {
                                 case 'hEditorTheme':
+                                    data = {...data, src: value.thumbnail, href: value.link};
                                     break;
                                 case 'hEditorTheme3':
+                                    data = {...data, src: value.thumbnail, href: value.link};
                                     break;
                                 case 'hSeasonTheme':
+                                    data = {...data, src: value.thumbnail, href: value.link};
                                     break;
                                 case 'h100':
+                                    data = {...data, src: value.thumbnail, href: value.link};
                                     break;
                                 case 'hAlbum':
+                                    data = {...data, src: value.thumbnail, href: value.link};
                                     break;
                                 default:
                                     console.log('No section id!');
                                     break;
                             }
+                            indexKey = `${item.sectionType}_${item.sectionId}`;
                             break;
                         default:
                             console.log('No section type!');
                             break;
                     }
-                    value.content = await super._renderEjsFile(itemPath, data);
+                    value.content = await super._renderEjsFile(itemsPath, data);
                     content += value.content;
                 }
             }
 
-            item.content = await super._renderEjsFile(sectionTypePath, { content: content });
+            sectionTypeData.title = item.title;
+            sectionTypeData.content = content;
+
+            item.content = await super._renderEjsFile(sectionTypePath, sectionTypeData);
             contentSectionType[indexKey] = item.content;
         }
 
